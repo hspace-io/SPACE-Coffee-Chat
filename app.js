86cc140e01ac6cb67d67f61cf5372a01f059a8e0
tkdgus721 <tkdgus782898@gmail.com>
2025-10-21 15:05:09 +0900

    lasstst

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const reservationRoutes = require("./routes/reservations.js");
 
 const app = express();
 
-// CORS 허용
-app.use(cors({ origin: "*" }));
+// 🔹 CORS 설정
+app.use(cors({
+  origin: ["http://192.168.10.140:3000"], // 프론트 주소
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
+
+// JSON 바디 파싱
 app.use(express.json());
 
 // 라우트 연결
 app.use("/api/reservations", reservationRoutes);
+
+// OPTIONS preflight 요청 자동 응답
+app.options("*", cors());
 
 module.exports = app;
