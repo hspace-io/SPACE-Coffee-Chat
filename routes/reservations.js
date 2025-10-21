82e579882e068705afbb166b8b6bccfae15768a6
tkdgus721 <tkdgus782898@gmail.com>
2025-10-21 09:45:36 +0900

    err

--- routes/reservations.js
+++ routes/reservations.js
@@ -3,11 +3,12 @@ const express = require("express");
 const router = express.Router();
 const reservationController = require(path.join(__dirname, "..", "controllers", "reservationController.js"));
 
-// 🔹 임시 테스트용 라우트 (추가)
+// 🔹 임시 테스트용 라우트
 router.get("/test", (req, res) => {
   res.json([{ _id: "test", startTime: new Date() }]);
 });
 
+// 정상 예약 라우트
 router.get("/", reservationController.getAllReservations);
 router.post("/", reservationController.createReservation);
 router.post("/:reservationId/apply", reservationController.applyReservation);
