d63b0bf5954262142daead834ad060cdfece1f3a
tkdgus721 <tkdgus782898@gmail.com>
2025-10-22 12:09:25 +0900

    poiu

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-// app.js
 const express = require("express");
 const cors = require("cors");
 const reservationRoutes = require("./routes/reservations.js");
 
 const app = express();
 
-// 🔹 CORS 설정 (origin에는 경로 붙이지 말 것)
-const corsOptions = {
-  origin: "http://192.168.10.140:3000", // 프론트 도메인 + 포트만
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+// 🔹 배포 환경 CORS 설정
+const allowedOrigins = ["http://192.168.10.140:3000"];
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS policy: Origin not allowed"));
+    }
+  },
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
+  credentials: true
+}));
 
 // 🔹 Preflight 요청 처리
-app.options("*", cors(corsOptions));
+app.options("*", cors());
 
 // 🔹 JSON 바디 파싱
 app.use(express.json());
